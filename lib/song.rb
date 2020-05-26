5cd6145dfd877c05ec1f5957f6f1d02ca2579795
Heath Mast <hrmast@gmail.com>
2020-05-26 03:26:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,45 @@
+require "pry"
+
 class Song
-attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
-@@count = 0
-@@artists = []
-@@genres = []
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@artist_count = {}
 
-def initialize(name, artist, genres)
-  @name = name
-  @artist = artist
-  @genre = genres
-  @@count += 1
-  @@artists << @artist
-  @@genres << @genre
-end
+  def initialize(name, artist, genres)
+    @name = name
+    @artist = artist
+    @genre = genres
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+    @@genre_count
+    @@artist_count
+  end
 
-def self.count
-  @@count
-end
+  def self.count
+    @@count
+  end
 
-def self.artists
-  @@artist
-end
+  def self.artists
+    @@artists.uniq
+  end
 
-def self.genres
-  @@genres
-end
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    binding.pry
+    #count the number of times a song occurs within the genre
+
+    @name.count
+  end
+
+  def self.artist_count
+
+  end
 end
