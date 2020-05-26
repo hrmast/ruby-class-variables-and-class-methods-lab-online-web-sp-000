fe76e32a580c2e1372574400ac6210f0f4ecab0f
Heath Mast <hrmast@gmail.com>
2020-05-26 03:53:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,7 @@ class Song
     #count the number of times a song occurs within the genre
     genre_count = Hash.new(0)
     @@genres.each do |genre| genre_count[genre] += 1
+    genre_count
 
 
 
